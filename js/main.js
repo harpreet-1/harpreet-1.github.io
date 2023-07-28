39a863c451933c743c13b6cd66966bfe404825ca
harpreet-1 <hpsllg8@gmail.com>
2023-07-28 07:47:21 +0530

    done

--- js/main.js
+++ js/main.js
@@ -93,7 +93,7 @@
       $(".navbar-expand-md").removeClass("navbar-trans");
     } else {
       $(".navbar-expand-md").addClass("navbar-trans");
-      $(".navbar-expand-md").removeClass("navbar-reduce");
+      // $(".navbar-expand-md").removeClass("navbar-reduce");
     }
     if ($(window).scrollTop() > top) {
       $(".scrolltop-mf").fadeIn(1000, "easeInOutExpo");
